53ea4f1c40c4ecd2cc173d58e7dc679b138a9042
Misha Topchilo <miksoft.tm@gmail.com>
2022-12-03 20:35:05 +0500

    Added drawer menu

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { store } from './api/store';
 
 import {LocalMapScreen} from "./screens/local-map/LocalMapScreen";
 import {ListPlacesScreen} from "./screens/list-places/ListPlacesScreen";
-import {Text, Button, StatusBar} from "react-native";
+import {StatusBar} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import CustomDrawer from "./components/custom-drawer/CustomDrawer";
 
 export type RootStackParamList = {
     List: undefined;
     Map: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator<RootStackParamList>();
 
 export default function App() {
     React.useEffect(() => {
@@ -22,26 +26,51 @@ export default function App() {
     });
 
     return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="List"
-                        component={ListPlacesScreen}
-                        options={{
-                            headerTitle: (props) => <Text>List places</Text>,
-                            headerRight: () => (
-                                <Button
-                                    onPress={() => alert('This is a button!')}
-                                    title="Info"
-                                    color="#fff"
-                                />
-                            ),
-                        }}
-                    />
-                    <Stack.Screen name="Map" options={{title: 'Local Map'}} component={LocalMapScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </Provider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Provider store={store}>
+                <NavigationContainer>
+                    <Drawer.Navigator
+                        drawerContent={props => <CustomDrawer {...props} />}
+                        screenOptions={{
+                            headerShown: false,
+                            drawerActiveBackgroundColor: '#aa18ea',
+                            drawerActiveTintColor: '#fff',
+                            drawerInactiveTintColor: '#333',
+                            drawerLabelStyle: {
+                                marginLeft: -25,
+                                fontFamily: 'Roboto-Medium',
+                                fontSize: 15,
+                            },
+                        }}>
+                        <Drawer.Screen
+                            name={'List'}
+                            component={ListPlacesScreen}
+                            options={{
+                                drawerIcon: ({color}) => (
+                                    <Ionicons
+                                        name={'home-outline'}
+                                        size={22}
+                                        color={color}
+                                    />
+                                ),
+                            }}
+                        />
+                        <Drawer.Screen
+                            name={'Map'}
+                            component={LocalMapScreen}
+                            options={{
+                                drawerIcon: ({color}) => (
+                                    <Ionicons
+                                        name={'person-outline'}
+                                        size={22}
+                                        color={color}
+                                    />
+                                ),
+                            }}
+                        />
+                    </Drawer.Navigator>
+                </NavigationContainer>
+            </Provider>
+        </GestureHandlerRootView>
     );
 }
