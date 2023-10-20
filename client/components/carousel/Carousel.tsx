b21dcce688355c50f90a0f84eeea914596c836ea
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-10-20 15:02:35 -0700

    Updated Carousel

--- client/components/carousel/Carousel.tsx
+++ client/components/carousel/Carousel.tsx
@@ -1,6 +1,10 @@
-import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react'
-import React from 'react'
+import useEmblaCarousel, {
+    EmblaCarouselType,
+    EmblaOptionsType
+} from 'embla-carousel-react'
+import React, { useCallback, useEffect, useState } from 'react'
 
+import { NextButton, PrevButton } from './CarouselButtons'
 import styles from './styles.module.sass'
 
 interface CarouselProps {
@@ -9,15 +13,50 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ options, children }) => {
-    const [emblaRef] = useEmblaCarousel(options)
+    const [emblaRef, emblaApi] = useEmblaCarousel(options)
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+
+    const scrollPrev = useCallback(
+        () => emblaApi && emblaApi.scrollPrev(),
+        [emblaApi]
+    )
+    const scrollNext = useCallback(
+        () => emblaApi && emblaApi.scrollNext(),
+        [emblaApi]
+    )
+
+    const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+        setPrevBtnDisabled(!emblaApi.canScrollPrev())
+        setNextBtnDisabled(!emblaApi.canScrollNext())
+    }, [])
+
+    useEffect(() => {
+        if (!emblaApi) return
+
+        onSelect(emblaApi)
+        emblaApi.on('reInit', onSelect)
+        emblaApi.on('select', onSelect)
+    }, [emblaApi, onSelect])
 
     return (
-        <div className={styles.embla}>
+        <div className={styles.carousel}>
             <div
-                className={styles.embla__viewport}
+                className={styles.viewport}
                 ref={emblaRef}
             >
-                <div className={styles.embla__container}>{children}</div>
+                <div className={styles.container}>{children}</div>
+            </div>
+
+            <div className={styles.buttonsContainer}>
+                <PrevButton
+                    onClick={scrollPrev}
+                    disabled={prevBtnDisabled}
+                />
+                <NextButton
+                    onClick={scrollNext}
+                    disabled={nextBtnDisabled}
+                />
             </div>
         </div>
     )
