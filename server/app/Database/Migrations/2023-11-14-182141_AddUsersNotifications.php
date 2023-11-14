88616bf17484168d1d9aed2dda344a2d166ae3df
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-11-14 10:24:43 -0800

    Created AddUsersNotifications

---
+++ server/app/Database/Migrations/2023-11-14-182141_AddUsersNotifications.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddUsersNotifications extends Migration {
+    public function up() {
+        $this->forge->addField([
+            'id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+                'null'       => false,
+                'unique'     => true
+            ],
+            'type' => [
+                'type' => 'ENUM("level", "achievements", "rating", "comment")',
+                'null' => false,
+            ],
+            'user_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+                'null'       => true
+            ],
+            'object_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+                'null'       => true
+            ],
+            'created_at DATETIME default current_timestamp',
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('users_notifications');
+    }
+
+    public function down() {
+        $this->forge->dropTable('users_notifications');
+    }
+}
