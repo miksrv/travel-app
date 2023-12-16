f6b76012333099b8a1d0969dad34fe33c3d9cf22
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-16 10:00:55 -0800

    Improved UI component - InitializedMDXEditor

--- client/components/form-controllers/content-editor/InitializedMDXEditor.tsx
+++ client/components/form-controllers/content-editor/InitializedMDXEditor.tsx
@@ -22,38 +22,38 @@ import type { ForwardedRef } from 'react'
 
 import styles from './styles.module.sass'
 
-export default function InitializedMDXEditor({
+const InitializedMDXEditor = ({
     editorRef,
     ...props
-}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
-    return (
-        <MDXEditor
-            className={styles.editor}
-            contentEditableClassName={styles.markdownEditorContent}
-            plugins={[
-                diffSourcePlugin({
-                    diffMarkdown: props.markdown,
-                    viewMode: 'rich-text'
-                }),
-                headingsPlugin(),
-                listsPlugin(),
-                linkPlugin(),
-                imagePlugin(),
-                quotePlugin(),
-                thematicBreakPlugin(),
-                markdownShortcutPlugin(),
-                toolbarPlugin({
-                    toolbarContents: () => (
-                        <>
-                            <DiffSourceToggleWrapper>
-                                <UndoRedo />
-                            </DiffSourceToggleWrapper>
-                        </>
-                    )
-                })
-            ]}
-            {...props}
-            ref={editorRef}
-        />
-    )
-}
+}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) => (
+    <MDXEditor
+        className={styles.editor}
+        contentEditableClassName={styles.markdownEditorContent}
+        plugins={[
+            diffSourcePlugin({
+                diffMarkdown: props.markdown,
+                viewMode: 'rich-text'
+            }),
+            headingsPlugin(),
+            listsPlugin(),
+            linkPlugin(),
+            imagePlugin(),
+            quotePlugin(),
+            thematicBreakPlugin(),
+            markdownShortcutPlugin(),
+            toolbarPlugin({
+                toolbarContents: () => (
+                    <>
+                        <DiffSourceToggleWrapper>
+                            <UndoRedo />
+                        </DiffSourceToggleWrapper>
+                    </>
+                )
+            })
+        ]}
+        {...props}
+        ref={editorRef}
+    />
+)
+
+export default InitializedMDXEditor
