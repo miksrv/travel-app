3146bcd6236f792944da6582bdcd7ef81149bacb
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-01-04 11:51:36 -0800

    Implemented new function for PageLayout and Header components

--- client/components/page-layout/PageLayout.tsx
+++ client/components/page-layout/PageLayout.tsx
@@ -1,7 +1,14 @@
+import {
+    ArticleOutlined,
+    MapOutlined,
+    PeopleOutline,
+    PhotoOutlined,
+    TerrainOutlined
+} from '@mui/icons-material'
+import Link from 'next/link'
 import React, { useState } from 'react'
 
-import { toggleOverlay } from '@/api/applicationSlice'
-import { useAppDispatch, useAppSelector } from '@/api/store'
+import { useAppSelector } from '@/api/store'
 
 import Header from '@/components/header'
 
@@ -16,38 +23,92 @@ interface PageLayoutProps {
     children?: React.ReactNode
 }
 
+type MenuItemsProps = {
+    link: string
+    text: string
+    icon?: React.ReactNode
+}
+
+const menuItems: MenuItemsProps[] = [
+    {
+        icon: <ArticleOutlined color={'primary'} />,
+        link: '/',
+        text: 'Лента активностей'
+    },
+    {
+        icon: <TerrainOutlined color={'primary'} />,
+        link: '/places',
+        text: 'Интересные места'
+    },
+    // {
+    //     icon: <PlaceOutlined color={'primary'} />,
+    //     link: '/places',
+    //     text: 'Населенные пункты'
+    // },
+    {
+        icon: <MapOutlined color={'primary'} />,
+        link: '/map',
+        text: 'Карта мест'
+    },
+    // {
+    //     icon: <LabelOutlined color={'primary'} />,
+    //     link: '/tags',
+    //     text: 'Метки мест'
+    // },
+    {
+        icon: <PhotoOutlined color={'primary'} />,
+        link: '/photos',
+        text: 'Фотографии'
+    },
+    // {
+    //     icon: <BookmarkBorderOutlined color={'primary'} />,
+    //     link: '/categories',
+    //     text: 'Категории'
+    // },
+    {
+        icon: <PeopleOutline color={'primary'} />,
+        link: '/users',
+        text: 'Путешественники'
+    }
+]
+
 const PageLayout: React.FC<PageLayoutProps> = ({
     title,
     breadcrumb,
     fullSize,
     children
 }) => {
-    const dispatch = useAppDispatch()
     const application = useAppSelector((store) => store.application)
 
-    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
 
-    const handleToggleSidebar = () => {
-        dispatch(toggleOverlay(!sidebarOpen))
-        setSidebarOpen(!sidebarOpen)
+    const handleCloseOverlay = () => {
+        setSidebarOpen(false)
+    }
+
+    const handleOpenSideBar = () => {
+        setSidebarOpen(true)
     }
 
     return (
         <div className={cn(styles.component, fullSize && styles.fullSize)}>
             <div
+                role={'button'}
+                tabIndex={0}
                 className={cn(
                     styles.overlay,
                     application?.showOverlay || sidebarOpen
                         ? styles.displayed
                         : styles.hidden
                 )}
-                onClick={handleToggleSidebar}
+                onKeyDown={handleCloseOverlay}
+                onClick={handleCloseOverlay}
             />
             <Header
                 fullSize={fullSize}
                 title={title}
                 breadcrumb={breadcrumb}
-                onMenuClick={handleToggleSidebar}
+                onMenuClick={handleOpenSideBar}
             />
             <aside
                 className={cn(
@@ -55,7 +116,19 @@ const PageLayout: React.FC<PageLayoutProps> = ({
                     sidebarOpen ? styles.opened : styles.closed
                 )}
             >
-                Sidebar Content
+                <menu className={styles.menu}>
+                    {menuItems.map((item) => (
+                        <li key={item.link}>
+                            <Link
+                                href={item.link}
+                                title={item.text}
+                                onClick={handleCloseOverlay}
+                            >
+                                {item.text}
+                            </Link>
+                        </li>
+                    ))}
+                </menu>
             </aside>
             <main className={styles.wrapper}>{children}</main>
         </div>
