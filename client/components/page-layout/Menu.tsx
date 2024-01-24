00c88fa0d0861668e841d8f2a2e011376180da95
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-01-24 13:42:33 -0800

    Added links in the main menu

--- client/components/page-layout/Menu.tsx
+++ client/components/page-layout/Menu.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import Icon from '@/ui/icon'
 import { IconTypes } from '@/ui/icon/types'
 
+import { useAppSelector } from '@/api/store'
+
 import styles from './styles.module.sass'
 
 export type MenuItemType = {
@@ -12,43 +14,57 @@ export type MenuItemType = {
     text: string
 }
 
-export const menuItems: MenuItemType[] = [
-    {
-        icon: 'Map',
-        link: '/',
-        text: 'Карта интересных мест'
-    },
-    {
-        icon: 'Terrain',
-        link: '/places',
-        text: 'Интересные места'
-    },
-    {
-        icon: 'PlusCircle',
-        link: '/places/create',
-        text: 'Добавить место'
-    }
-]
-
 interface MenuProps {
     onClick?: () => void
 }
 
-const Menu: React.FC<MenuProps> = ({ onClick }) => (
-    <menu className={styles.menu}>
-        {menuItems.map((item) => (
-            <li key={item.link}>
-                <Link
-                    href={item.link}
-                    title={item.text}
-                    onClick={onClick}
-                >
-                    {item.icon && <Icon name={item.icon} />}
-                    {item.text}
-                </Link>
-            </li>
-        ))}
-    </menu>
-)
+const Menu: React.FC<MenuProps> = ({ onClick }) => {
+    const authSlice = useAppSelector((state) => state.auth)
+
+    const menuItems: MenuItemType[] = [
+        {
+            icon: 'User',
+            link: authSlice.isAuth ? `/users/${authSlice?.user?.id}` : '/login',
+            text: 'Моя страница'
+        },
+        {
+            icon: 'Map',
+            link: '/',
+            text: 'Карта интересных мест'
+        },
+        {
+            icon: 'Terrain',
+            link: '/places',
+            text: 'Интересные места'
+        },
+        {
+            icon: 'PlusCircle',
+            link: '/places/create',
+            text: 'Добавить место'
+        },
+        {
+            icon: 'Users',
+            link: '/users/',
+            text: 'Путешественники'
+        }
+    ]
+
+    return (
+        <menu className={styles.menu}>
+            {menuItems.map((item) => (
+                <li key={item.link}>
+                    <Link
+                        href={item.link}
+                        title={item.text}
+                        onClick={onClick}
+                    >
+                        {item.icon && <Icon name={item.icon} />}
+                        {item.text}
+                    </Link>
+                </li>
+            ))}
+        </menu>
+    )
+}
 
 export default Menu
