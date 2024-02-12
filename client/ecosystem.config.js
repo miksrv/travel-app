8ad197262f7b748324fd6e868b59ce61898ce7df
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-02-12 13:48:35 -0800

    Fixed Github CI/CD issue

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
             instances: 1,
             max_memory_restart: '1G',
             name: 'geometki.com',
-            script: './node_modules/next/dist/bin/next',
+            script: 'server.js',
             watch: false
         }
     ]
