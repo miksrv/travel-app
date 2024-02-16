cfb7e4b5104c77c018509d840dc267eab16e13e6
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-02-16 10:28:32 -0800

    Removed UI useEmblaCarousel

---
+++ client/components/_carousel/CarouselButtons.tsx
@@ -0,0 +1,40 @@
+// import React, { PropsWithChildren } from 'react'
+//
+// import styles from './styles.module.sass'
+//
+// type PropType = PropsWithChildren<
+//     React.DetailedHTMLProps<
+//         React.ButtonHTMLAttributes<HTMLButtonElement>,
+//         HTMLButtonElement
+//     >
+// >
+//
+// export const PrevButton: React.FC<PropType> = (props) => {
+//     const { children, ...restProps } = props
+//
+//     return (
+//         <button
+//             className={styles.arrowButtonLeft}
+//             type={'button'}
+//             {...restProps}
+//         >
+//             {/*<ArrowBackIosOutlined />*/}
+//             {children}
+//         </button>
+//     )
+// }
+//
+// export const NextButton: React.FC<PropType> = (props) => {
+//     const { children, ...restProps } = props
+//
+//     return (
+//         <button
+//             className={styles.arrowButtonRight}
+//             type={'button'}
+//             {...restProps}
+//         >
+//             {/*<ArrowForwardIosOutlined />*/}
+//             {children}
+//         </button>
+//     )
+// }
