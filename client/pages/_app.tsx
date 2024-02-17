c8ebd2e3c8b7a79555e22ed12b2ff1c2f428d298
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-02-17 09:27:02 -0800

    Added Yandex Metrika Counter

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,6 +6,7 @@ import utc from 'dayjs/plugin/utc'
 import { appWithTranslation, useTranslation } from 'next-i18next'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import React from 'react'
 import 'react-image-lightbox/style.css'
 import { Provider } from 'react-redux'
 
@@ -39,6 +40,11 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Provider store={store}>
                 <Component {...pageProps} />
             </Provider>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: '<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(96500810, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/96500810" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->'
+                }}
+            />
         </>
     )
 }
