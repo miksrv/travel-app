fbf4a7d24f0773fae20e35497bfadefba3c7fa1a
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-03-08 14:16:12 -0800

    Cache images, removed unused code

--- client/next.config.js
+++ client/next.config.js
@@ -3,10 +3,25 @@ const { i18n } = require('./next-i18next.config.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n,
-    // unoptimized - When true, the source image will be served as-is instead of changing quality,
-    // size, or format. Defaults to false.
     images: {
-        unoptimized: true
+        // https://nextjs.org/docs/pages/api-reference/components/image
+        remotePatterns: [
+            {
+                hostname: 'api.geometki.com',
+                pathname: '/uploads/**',
+                port: '',
+                protocol: 'https'
+            },
+            {
+                hostname: 'localhost',
+                pathname: '/uploads/**',
+                port: '8080',
+                protocol: 'http'
+            }
+        ],
+        // unoptimized - When true, the source image will be served as-is instead of changing quality,
+        // size, or format. Defaults to false.
+        unoptimized: false
     },
     output: 'standalone',
 
