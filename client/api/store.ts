ff52abf03b9b387d97b4c3f701396a6ee83d0383
Misha Topchilo <miksoft.tm@gmail.com>
2024-03-13 12:23:43 -0700

    Improved UI, removed unused styles (#100)
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- client/api/store.ts
+++ client/api/store.ts
@@ -1,11 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { createWrapper } from 'next-redux-wrapper'
-import {
-    TypedUseSelectorHook,
-    useDispatch,
-    useSelector,
-    useStore
-} from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 import applicationSlice from '@/api/applicationSlice'
 import authSlice from '@/api/authSlice'
@@ -31,6 +26,6 @@ export type AppDispatch = AppStore['dispatch']
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppStore: () => AppStore = useStore
+// export const useAppStore: () => AppStore = useStore
 
 export const wrapper = createWrapper<AppStore>(store, { debug: false })
--- server/app/Entities/User.php
+++ server/app/Entities/User.php
@@ -4,15 +4,17 @@ use CodeIgniter\Entity\Entity;
 
 class User extends Entity {
     protected $casts = [
+        'id'          => 'string',
         'name'        => 'string',
         'email'       => 'string',
-        'avatar'      => 'string',
-        'website'     => 'string',
         'password'    => 'string',
+        'auth_type'   => 'string',
         'locale'      => 'string',
         'level'       => 'integer',
         'experience'  => 'integer',
         'reputation'  => 'integer',
+        'website'     => 'string',
+        'avatar'      => 'string',
         'activity_at' => 'datetime',
 
         'updated' => 'datetime',
