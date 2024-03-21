a81183cbf22d1e4715b2f5d0cc88023786858821
Misha Topchilo <miksoft.tm@gmail.com>
2024-03-21 11:27:24 -0700

    Develop (#119)
    * Implemented scroll to top button area
    
    * Implemented scroll to top action
    
    ---------
    
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- client/components/app-layout/AppLayout.tsx
+++ client/components/app-layout/AppLayout.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from 'next-i18next'
 import NextNProgress from 'nextjs-progressbar'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import Dialog from '@/ui/dialog'
+import Icon from '@/ui/icon'
 
 import { closeAuthDialog } from '@/api/applicationSlice'
 import { useAppDispatch, useAppSelector } from '@/api/store'
@@ -33,9 +35,16 @@ const AppLayout: React.FC<AppLayoutProps> = ({
     fullSize,
     children
 }) => {
+    const { t } = useTranslation('common', {
+        keyPrefix: 'components.appLayout'
+    })
     const dispatch = useAppDispatch()
     const authSlice = useAppSelector((state) => state.auth)
     const application = useAppSelector((store) => store.application)
+
+    const [leftDistance, setLeftDistance] = useState<number>()
+    const [scrollTopVisible, setScrollTopVisible] = useState<boolean>(false)
+    const menuBarRef = useRef<HTMLDivElement>(null)
 
     const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
     const [authForm, setAuthForm] = useState<AuthFormType>('login')
@@ -52,6 +61,34 @@ const AppLayout: React.FC<AppLayoutProps> = ({
         setAuthForm('login')
         dispatch(closeAuthDialog())
     }
+
+    const handleScrollToTop = () => {
+        window?.scrollTo(0, 0)
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollTopVisible(window?.scrollY > 500)
+        }
+
+        const handleResize = () => {
+            if (menuBarRef.current) {
+                const rect = menuBarRef.current.getBoundingClientRect()
+
+                setLeftDistance(rect.left + rect?.width - 5)
+            }
+        }
+
+        handleResize()
+
+        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     useEffect(() => {
         if (application?.showOverlay || sidebarOpen) {
@@ -77,6 +114,23 @@ const AppLayout: React.FC<AppLayoutProps> = ({
                 color={'#2688eb'}
                 options={{ showSpinner: false }}
             />
+
+            <div
+                tabIndex={0}
+                role={'button'}
+                className={styles.scrollArea}
+                style={{
+                    display: scrollTopVisible ? 'block' : 'none',
+                    width: leftDistance
+                }}
+                onKeyDown={() => undefined}
+                onClick={handleScrollToTop}
+            >
+                <div className={styles.buttonToTop}>
+                    <Icon name={'Up'} />
+                    {t('scrollToTop')}
+                </div>
+            </div>
 
             <div
                 role={'button'}
@@ -132,7 +186,10 @@ const AppLayout: React.FC<AppLayoutProps> = ({
             </aside>
 
             <section className={styles.mainContainer}>
-                <aside className={styles.menubar}>
+                <aside
+                    className={styles.menubar}
+                    ref={menuBarRef}
+                >
                     <div className={styles.rails}>
                         <Menu
                             type={'desktop'}
