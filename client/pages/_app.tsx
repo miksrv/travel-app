b63c0d02823918a6203d8b0c4e214d5940dc7ca1
Misha Topchilo <miksoft.tm@gmail.com>
2024-04-19 16:29:38 -0700

    Improved UI content editor, upgraded UI libraries
    Co-authored-by: Misha Topchilo <20854498+miksrv@users.noreply.github.com>

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -46,7 +46,7 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Head>
                 <meta
                     name='viewport'
-                    content='width=device-width, initial-scale=1, shrink-to-fit=no'
+                    content='width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no'
                 />
                 <link
                     rel='apple-touch-icon'
