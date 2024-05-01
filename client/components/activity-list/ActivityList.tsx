cf1e8a85af144a91106d7aabe835658d9afb4c78
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-05-01 12:00:41 -0700

    Added loader for activity list

--- client/components/activity-list/ActivityList.tsx
+++ client/components/activity-list/ActivityList.tsx
@@ -4,6 +4,7 @@ import Markdown from 'react-markdown'
 import Gallery from 'react-photo-gallery'
 
 import Container from '@/ui/container'
+import Skeleton from '@/ui/skeleton'
 
 import { IMG_HOST } from '@/api/api'
 import { ActivityTypes, Item } from '@/api/types/Activity'
@@ -18,15 +19,63 @@ import styles from './styles.module.sass'
 
 interface PlacesListProps {
     activities?: Item[]
+    loading?: boolean
 }
 
-const ActivityList: React.FC<PlacesListProps> = ({ activities }) =>
-    activities?.map((item, index) => (
-        <ActivityListItem
-            key={`activity-${index}`}
-            item={item}
-        />
-    ))
+const ActivityList: React.FC<PlacesListProps> = ({ activities, loading }) => (
+    <>
+        {activities?.map((item, index) => (
+            <ActivityListItem
+                key={`activity-${index}`}
+                item={item}
+            />
+        ))}
+
+        {loading && (
+            <Container className={styles.activityContainer}>
+                <div
+                    className={styles.userAvatar}
+                    style={{ display: 'flex' }}
+                >
+                    <Skeleton
+                        style={{
+                            borderRadius: '50%',
+                            height: '32px',
+                            width: '32px'
+                        }}
+                    />
+                    <div style={{ marginLeft: '10px' }}>
+                        <Skeleton
+                            style={{
+                                height: '16px',
+                                marginBottom: '5px',
+                                width: '100px'
+                            }}
+                        />
+                        <Skeleton
+                            style={{
+                                height: '12px',
+                                width: '200px'
+                            }}
+                        />
+                    </div>
+                </div>
+                {Array(5)
+                    .fill('')
+                    .map((_, i) => (
+                        <Skeleton
+                            key={i}
+                            style={{
+                                height: '16px',
+                                marginBottom: '5px',
+                                width: i === 4 ? '75%' : '100%'
+                            }}
+                        />
+                    ))}
+            </Container>
+        )}
+    </>
+)
 
 interface ActivityListItemProps {
     item: Item
