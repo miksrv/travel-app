463c2af2b05105a94ed65c94ac16819635306a73
Misha Topchilo <miksoft.tm@gmail.com>
2026-08-03 21:18:16 -0700

    Refactor place sidebar metadata rendering
    Reworked `PlaceInfoSidebar` to render metadata from a typed `infoItems` array instead of repeated JSX, improving maintainability and consistency. The category row now uses `next/image` with `categoryImage` and a styled link (replacing `CategoryBadge`), and the coordinates icon was updated to `Position`. Also fixed `PlaceForm` imports by sourcing `PhotoGallery` from `@/components/widgets` while keeping `PhotoUploader` from shared components.

--- client/sections/place/place-info-sidebar/PlaceInfoSidebar.tsx
+++ client/sections/place/place-info-sidebar/PlaceInfoSidebar.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Container, Icon } from 'simple-react-ui-kit'
+import { Container, Icon, IconTypes } from 'simple-react-ui-kit'
 
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next/pages'
 
 import { ApiModel } from '@/api'
 import { CopyCoordinates, MapLinks, UserAvatar, UserAvatarGroup } from '@/components/shared'
-import { CategoryBadge } from '@/components/shared/category-badge'
+import { categoryImage } from '@/utils/categories'
 import { formatDate, formatThousands } from '@/utils/helpers'
 
 import styles from './styles.module.sass'
@@ -18,107 +19,107 @@ interface PlaceInfoSidebarProps {
     place?: ApiModel.Place
 }
 
+interface SidebarInfoItem {
+    icon: IconTypes
+    key: string
+    value: React.ReactNode
+}
+
 export const PlaceInfoSidebar: React.FC<PlaceInfoSidebarProps> = ({ place }) => {
     const { t } = useTranslation()
+
+    const infoItems: Array<SidebarInfoItem | false> = [
+        {
+            icon: 'Bookmark',
+            key: t('category'),
+            value: place?.category && (
+                <Link
+                    href={`/places?category=${place.category.name}`}
+                    title={`${place.category.title} - ${t('all-geotags-at-address')}`}
+                    className={styles.categoryLink}
+                >
+                    <Image
+                        src={categoryImage(place.category.name).src}
+                        alt={''}
+                        width={16}
+                        height={16}
+                    />
+                    {place.category.title}
+                </Link>
+            )
+        },
+        {
+            icon: 'User',
+            key: t('author'),
+            value: (
+                <UserAvatar
+                    user={place?.author}
+                    showName={true}
+                />
+            )
+        },
+        !!place?.editors?.length && {
+            icon: 'Users',
+            key: t('editors'),
+            value:
+                place.editors.length === 1 ? (
+                    <UserAvatar
+                        user={place.editors[0]}
+                        showName={true}
+                    />
+                ) : (
+                    <UserAvatarGroup
+                        size={'small'}
+                        users={place.editors}
+                    />
+                )
+        },
+        {
+            icon: 'Eye',
+            key: t('views'),
+            value: formatThousands(place?.views || 0)
+        },
+        !!place?.created && {
+            icon: 'Time',
+            key: t('created'),
+            value: formatDate(place.created.date, t('date-time-format'))
+        },
+        !!place?.updated && {
+            icon: 'Time',
+            key: t('edited'),
+            value: formatDate(place.updated.date, t('date-time-format'))
+        },
+        !!place?.distance && {
+            icon: 'Ruler',
+            key: t('distance-to-me'),
+            value: `${formatThousands(place.distance)} ${t('km')}`
+        },
+        {
+            icon: 'Position',
+            key: t('coordinates'),
+            value: (
+                <CopyCoordinates
+                    lat={place?.lat || 0}
+                    lon={place?.lon || 0}
+                />
+            )
+        }
+    ]
 
     return (
         <Container className={styles.component}>
             <ul>
-                <li>
-                    <Icon name={'Bookmark'} />
-                    <div className={styles.info}>
-                        <div className={styles.key}>{t('category')}</div>
-                        <div className={styles.value}>
-                            {place?.category && (
-                                <Link
-                                    href={`/places?category=${place.category.name}`}
-                                    title={`${place.category.title} - ${t('all-geotags-at-address')}`}
-                                >
-                                    <CategoryBadge category={place.category} />
-                                </Link>
-                            )}
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <Icon name={'User'} />
-                    <div className={styles.info}>
-                        <div className={styles.key}>{t('author')}</div>
-                        <div className={styles.value}>
-                            <UserAvatar
-                                user={place?.author}
-                                showName={true}
-                            />
-                        </div>
-                    </div>
-                </li>
-                {!!place?.editors?.length && (
-                    <li>
-                        <Icon name={'Users'} />
-                        <div className={styles.info}>
-                            <div className={styles.key}>{t('editors')}</div>
-                            <div className={styles.value}>
-                                {place.editors.length === 1 ? (
-                                    <UserAvatar
-                                        user={place.editors[0]}
-                                        showName={true}
-                                    />
-                                ) : (
-                                    <UserAvatarGroup
-                                        size={'small'}
-                                        users={place.editors}
-                                    />
-                                )}
+                {infoItems
+                    .filter((item): item is SidebarInfoItem => !!item)
+                    .map((item) => (
+                        <li key={item.key}>
+                            <Icon name={item.icon} />
+                            <div className={styles.info}>
+                                <div className={styles.key}>{item.key}</div>
+                                <div className={styles.value}>{item.value}</div>
                             </div>
-                        </div>
-                    </li>
-                )}
-                <li>
-                    <Icon name={'Eye'} />
-                    <div className={styles.info}>
-                        <div className={styles.key}>{t('views')}</div>
-                        <div className={styles.value}>{formatThousands(place?.views || 0)}</div>
-                    </div>
-                </li>
-                {place?.created && (
-                    <li>
-                        <Icon name={'Time'} />
-                        <div className={styles.info}>
-                            <div className={styles.key}>{t('created')}</div>
-                            <div className={styles.value}>{formatDate(place.created.date, t('date-time-format'))}</div>
-                        </div>
-                    </li>
-                )}
-                {place?.updated && (
-                    <li>
-                        <Icon name={'Time'} />
-                        <div className={styles.info}>
-                            <div className={styles.key}>{t('edited')}</div>
-                            <div className={styles.value}>{formatDate(place.updated.date, t('date-time-format'))}</div>
-                        </div>
-                    </li>
-                )}
-                {place?.distance && (
-                    <li>
-                        <Icon name={'Ruler'} />
-                        <div className={styles.info}>
-                            <div className={styles.key}>{t('distance-to-me')}</div>
-                            <div className={styles.value}>{`${formatThousands(place.distance)} ${t('km')}`}</div>
-                        </div>
-                    </li>
-                )}
-                <li>
-                    <Icon name={'Point'} />
-                    <div className={styles.info}>
-                        <div className={styles.key}>{t('coordinates')}</div>
-                        <div className={styles.value}>
-                            <CopyCoordinates
-                                lat={place?.lat || 0}
-                                lon={place?.lon || 0}
-                            />
-                        </div>
-                    </div>
-                </li>
+                        </li>
+                    ))}
             </ul>
 
             {place?.id && place.category && (
